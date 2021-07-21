55293345688ba6d921ffa54f8356515758049346
Prashant Dahal <prashantdah41@gmail.com>
2021-07-21 16:31:14 -0400

    Create an android app with empty activity

---
+++ app/src/main/java/com/example/yourdailynews/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.yourdailynews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
